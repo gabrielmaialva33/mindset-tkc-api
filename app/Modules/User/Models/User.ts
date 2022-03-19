a7b861bc89dbedfea5f149c3300ac7a63511c523
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-03-19 01:06:45 -0300

    chore: configure adonis auth

---
+++ app/Modules/User/Models/User.ts
@@ -0,0 +1,61 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  /**
+   * ------------------------------------------------------
+   * Columns
+   * ------------------------------------------------------
+   * - column typing struct
+   */
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public first_name: string
+
+  @column()
+  public last_name?: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public rememberMeToken?: string
+
+  @column.dateTime({ autoCreate: true })
+  public created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updated_at: DateTime
+
+  /**
+   * ------------------------------------------------------
+   * Relationships
+   * ------------------------------------------------------
+   * - define User model relationships
+   */
+
+  /**
+   * ------------------------------------------------------
+   * Hooks
+   * ------------------------------------------------------
+   * - define auto behaviors
+   */
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+
+  /**
+   * ------------------------------------------------------
+   * Query Scopes
+   * ------------------------------------------------------
+   */
+}
