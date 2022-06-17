ca203b1552c926bb30e27a06ef52dc4d590671b3
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-06-17 07:30:05 -0300

    feat: seed choices

--- database/migrations/1648364931662_create_default_choices.ts
+++ database/migrations/1648364931662_create_default_choices.ts
@@ -5,10 +5,14 @@ import { StoreDefaultChoicesService } from 'App/Modules/Question/Services/Choice
 
 import {
   AssertividadeChoicesDefault,
+  BinaryChoicesDefault,
   CerebralChoicesDefault,
   GerencialChoicesDefault,
   LikertChoicesDefault,
+  MeasurableChoicesDefault,
   MotivadoresChoicesDefault,
+  OpinionChoicesDefault,
+  TimeChoicesDefault,
 } from 'App/Modules/Question/Defaults/ChoicesDefault'
 
 export default class CreateDefaultChoices extends BaseSchema {
@@ -20,6 +24,10 @@ export default class CreateDefaultChoices extends BaseSchema {
     await storeDefaultChoices.init(GerencialChoicesDefault, 4, true)
     await storeDefaultChoices.init(CerebralChoicesDefault, 5, true)
     await storeDefaultChoices.init(LikertChoicesDefault, 6)
+    await storeDefaultChoices.init(TimeChoicesDefault, 7)
+    await storeDefaultChoices.init(OpinionChoicesDefault, 8)
+    await storeDefaultChoices.init(BinaryChoicesDefault, 9)
+    await storeDefaultChoices.init(MeasurableChoicesDefault, 10)
   }
 
   public async down() {
