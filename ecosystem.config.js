a6deadc6a09de8eb8c05d0b2fc280a36b6755e2e
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-06-27 17:50:29 -0300

    feat: add pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,6 @@ module.exports = {
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
-      env_production: {
-        NODE_ENV: 'production',
-      },
-      env_development: {
-        NODE_ENV: 'development',
-      },
     },
   ],
 }
