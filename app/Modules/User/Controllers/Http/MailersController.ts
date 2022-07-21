35f2b1385c9f991ddddeca8a43b4ea74d87a5e06
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-07-21 20:29:40 -0300

    feat: update code

--- app/Modules/User/Controllers/Http/MailersController.ts
+++ app/Modules/User/Controllers/Http/MailersController.ts
@@ -8,21 +8,21 @@ export default class MailersController {
     const { id: userId } = params
     const user = await User.query().where('id', userId).first()
     if (user) {
-      //if (!user.was_sent_email)
-      await Mail.send((message) => {
-        message
-          .subject('Relatório TCK')
-          .from('mindset-tkc@mrootx.xyz', 'TKC')
-          .to(user.email, user.name)
-          .htmlView('emails/welcome', {
-            name: user.name,
-            email: user.email,
-            birth_date: user.birth_date ? user.birth_date.toLocaleString().slice(0, 10) : null,
-            daily_workload: user.daily_workload,
-            retirement: user.retirement,
-            url: `https://mindset-tkc.mrootx.xyz/feedback/${userId}`,
-          })
-      })
+      if (!user.was_sent_email)
+        await Mail.send((message) => {
+          message
+            .subject('Relatório TCK')
+            .from('mindset-tkc@mrootx.xyz', 'TKC')
+            .to(user.email, user.name)
+            .htmlView('emails/welcome', {
+              name: user.name,
+              email: user.email,
+              birth_date: user.birth_date ? user.birth_date.toLocaleString().slice(0, 10) : null,
+              daily_workload: user.daily_workload,
+              retirement: user.retirement,
+              url: `https://mindset-tkc.mrootx.xyz/feedback/${userId}`,
+            })
+        })
 
       user.was_sent_email = true
       await user.save()
